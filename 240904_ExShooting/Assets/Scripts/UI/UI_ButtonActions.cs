33a12045af5580934fde5777c6f53946a47c6225
masic0729 <pihlan0620@gmail.com>
2024-09-07 22:08:57 +0900

    재업로드(진짜본)
    UI랑 폴더정리 진짜

--- 240904_ExShooting/Assets/Scripts/UI/UI_ButtonActions.cs
+++ 240904_ExShooting/Assets/Scripts/UI/UI_ButtonActions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rigi : MonoBehaviour
+public class UI_ButtonActions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +15,6 @@ public class rigi : MonoBehaviour
     {
         
     }
+
+    
 }
